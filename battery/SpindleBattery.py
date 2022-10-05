e91f86c7c68dc5ea06f5e37fb2ad7e73c70d10b3
Benjami Zhao <benzql@gmail.com>
2022-10-05 22:39:34 +1100

    Refractor Code

---
+++ battery/SpindleBattery.py
@@ -0,0 +1,16 @@
+import py_compile
+from battery.Battery import Battery
+import datetime
+
+class SpindleBattery(Battery):
+    def __init__(self, last_service_date: datetime.date, current_date: datetime.date ):
+        self.last_service_date= last_service_date
+        self.current_date = current_date
+        
+        self.SERVICE_THRESHOLD = 2
+    
+    def need_service(self)->bool:
+        if self.current_date.year - self.last_service_date.year >= self.SERVICE_THRESHOLD:
+            return True
+        else:
+            return False
---
+++ engine/WilloughbyEngine.py
@@ -0,0 +1,14 @@
+from engine.Engine import Engine
+
+class WilloughbyEngine (Engine):
+    def __init__(self, last_service_mileage:int, current_mileage:int):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+        
+        self.SERVICE_THRESHOLD =  60000
+    
+    def needs_service(self)->bool:
+        if self.current_mileage - self.last_service_mileage >= self.SERVICE_THRESHOLD:
+            return True
+        else:
+            return False
